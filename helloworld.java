d544919e06762e8056bf9c8a3efde1148e14feaf
harshk0525 <harshk0525@gmail.com>
2017-09-04 16:12:33 +0530

    Removing extra lines of code

--- helloworld.java
+++ helloworld.java
@@ -2,10 +2,7 @@ public class helloworld
 {
 	public static void main(String [] args) {
 		System.out.println("Hello World");
-<<<<<<< HEAD
 		System.out.println("I am the best");
-=======
 		System.out.println("Hello Ashoka");
->>>>>>> origin/harshk0525-patch-1
 	}
 } 
