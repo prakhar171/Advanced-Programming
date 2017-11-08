162fbfdfa01c522738df5f695a00c4a4ffe02f9f
prakhar171 <prakhar.jain_ug19@ashoka.edu.in>
2017-11-08 18:02:40 +0530

    Shuttle Scheduler Working

--- Ashoka CMS/Backend/ShuttleBooking/Shuttle.py
+++ Ashoka CMS/Backend/ShuttleBooking/Shuttle.py
@@ -5,74 +5,89 @@ import mysql.connector
 from mysql.connector import errorcode
 # Import modules for CGI handling 
 import cgi, cgitb
-
-
 # import datetime
 # weekno = datetime.datetime.today().weekday()
-
-
-
 # Create instance of FieldStorage 
 # print "<a href=/programs/SignUp.html>Re-Register</a>"
-
 form = cgi.FieldStorage() 
 cnn=mysql.connector.connect(user = "root", password = "", host = "localhost", database = "Rooms")
 cursor=cnn.cursor()
 name = form.getvalue('Name')
 ashoka_id = form.getvalue('Ashoka_ID')
-
 shuttle_time = form.getvalue('Shuttle_Time')
 origin = form.getvalue('Origin')
-
 print name, ashoka_id, shuttle_time, origin
-
 id=form.getvalue('id')
 print "<html>"
 print "<head>"
 print "<title>Shuttle Bookings</title>"
 print "</head>"
-
 addName="""INSERT INTO Bookings (Name,Ashoka_ID,Shuttle_Time,Origin) VALUES ('%s','%s','%s','%s')""" % (name,ashoka_id,shuttle_time,origin)
-
-
 cursor.execute(addName)
 cnn.commit()
-
-
 weekno = datetime.datetime.today().weekday()
 weekno+=1
-
-
 if weekno<4 and origin=="Ashoka":
 	check_availability = "SELECT  `%s` from Weekday_Ashoka" % (shuttle_time,)
 	cursor.execute(check_availability)
 	check_availability = cursor.fetchall()
 	cnn.commit()
-	print check_availability
 	s_time=str(shuttle_time)
-'''
-	if len(check_availability)!=20:
-
-		book_seat="""INSERT INTO Weekday_Ashoka (%s_time) VALUES ('%s',)""" % (shuttle_time,name,)
+	if len(check_availability)!=20:	
+		print len(check_availability)	
+		book_seat="""INSERT INTO Weekday_Ashoka (`%s`) VALUES ('%s')""" % (shuttle_time,name)
 		cursor.execute(book_seat)
 		cnn.commit()
-
 elif weekno<4 and Origin == "Jahangirpuri": 
-	book_shuttle = """INSERT INTO Weekday_Jahangirpuri (Name,Ashoka_ID,Time,Origin) VALUES ('%s','%s','%s','%s',)""" % (name, Ashoka_ID, Shuttle_Time, Origin)
+	check_availability = "SELECT  `%s` from Weekday_Jahangirpuri" % (shuttle_time,)
+	cursor.execute(check_availability)
+	check_availability = cursor.fetchall()
+	cnn.commit()
+	s_time=str(shuttle_time)
+	if len(check_availability)!=20:
+		book_seat="""INSERT INTO Weekday_Jahangirpuri (`%s`) VALUES ('%s')""" % (shuttle_time,name)
+		cursor.execute(book_seat)
+		cnn.commit()
 elif weekno==5 and Origin == "Ashoka":
-	book_shuttle = """INSERT INTO Friday_Ashoka (Name,Ashoka_ID,Time,Origin) VALUES ('%s','%s','%s','%s',)""" % (name, Ashoka_ID, Shuttle_Time, Origin)
+	check_availability = "SELECT  `%s` from Friday_Ashoka" % (shuttle_time,)
+	cursor.execute(check_availability)
+	check_availability = cursor.fetchall()
+	cnn.commit()
+	s_time=str(shuttle_time)
+	if len(check_availability)!=20:
+		book_seat="""INSERT INTO Friday_Ashoka (`%s`) VALUES ('%s')""" % (shuttle_time,name)
+		cursor.execute(book_seat)
+		cnn.commit()
 elif weekno==5 and Origin == "Jahangirpuri":
-	book_shuttle = """INSERT INTO Friday_Jahangirpuri (Name,Ashoka_ID,Time,Origin) VALUES ('%s','%s','%s','%s',)""" % (name, Ashoka_ID, Shuttle_Time, Origin)
+	check_availability = "SELECT  `%s` from Friday_Jahangirpuri" % (shuttle_time,)
+	cursor.execute(check_availability)
+	check_availability = cursor.fetchall()
+	cnn.commit()
+	s_time=str(shuttle_time)
+	if len(check_availability)!=20:
+		book_seat="""INSERT INTO Friday_Jahangirpuri (`%s`) VALUES ('%s')""" % (shuttle_time,name)
+		cursor.execute(book_seat)
+		cnn.commit()
 elif weekno>5 and Origin=="Ashoka":
-	book_shuttle = """INSERT INTO Weekend_Ashoka (Name,Ashoka_ID,Time,Origin) VALUES ('%s','%s','%s','%s',)""" % (name, Ashoka_ID, Shuttle_Time, Origin)
+	check_availability = "SELECT  `%s` from Weekend_Ashoka" % (shuttle_time,)
+	cursor.execute(check_availability)
+	check_availability = cursor.fetchall()
+	cnn.commit()
+	s_time=str(shuttle_time)
+	if len(check_availability)!=20:
+		book_seat="""INSERT INTO Weekend_Ashoka (`%s`) VALUES ('%s')""" % (shuttle_time,name)
+		cursor.execute(book_seat)
+		cnn.commit()
 else:
-    book_shuttle = """INSERT INTO Weekend_Jahangirpuri (Name,Ashoka_ID,Time,Origin) VALUES ('%s','%s','%s','%s',)""" % (name, Ashoka_ID, Shuttle_Time, Origin)
-
-
-'''
-
+	check_availability = "SELECT  `%s` from Weekend_Jahangirpuri" % (shuttle_time,)
+	cursor.execute(check_availability)
+	check_availability = cursor.fetchall()
+	cnn.commit()
+	s_time=str(shuttle_time)
+	if len(check_availability)!=20:
+		book_seat="""INSERT INTO Friday_Ashoka (`%s`) VALUES ('%s')""" % (shuttle_time,name)
+		cursor.execute(book_seat)
+		cnn.commit()
 cnn.close()
-
-
 print "</body>"
 print "</html>"
